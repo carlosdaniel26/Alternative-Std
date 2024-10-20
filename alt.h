e5818d5508222f0afcdca7f4fbfbfc8e73c641ed
Carlos <105173667+carlosdaniel26@users.noreply.github.com>
2024-10-19 22:44:05 -0300

    alt_isalpha implemented

---
+++ alt.h
@@ -0,0 +1,1 @@
+int	alt_isalpha(int c);
---
+++ alt_isalpha.c
@@ -0,0 +1,9 @@
+int	alt_isalpha(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return (1);
+    if (c >= 'a' && c <='z')
+        return (2);
+    
+    return (0);
+}
